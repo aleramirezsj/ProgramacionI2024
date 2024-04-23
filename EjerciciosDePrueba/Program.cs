8ef8942773c16a7619939a03013b5b6de6dd0449
aleramirezsj <aleramirezsj@gmail.com>
2024-04-23 16:51:24 -0300

    Agregar archivos de proyecto.

---
+++ EjerciciosDePrueba/Program.cs
@@ -0,0 +1,73 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //PedirNombreYSaludar();
+        //CalcularDiasDeVida();
+        ImprimirMinimoDe4();
+    }
+
+    /// <summary>
+    /// algoritmo que imprime el mínimo entre cuatro valores que se piden al usuario.
+    /// </summary>
+    private static void ImprimirMinimoDe4()
+    {
+        //declaramos las variables
+        int num1, num2, num3, num4;
+
+        //pedimos el ingreso de los 4 números
+        Console.Write("Ingrese el primer número:");
+        num1= int.Parse(Console.ReadLine());
+        
+        Console.Write("Ingrese el segundo número:");
+        num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer número:");
+        num3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el cuarto número:");
+        num4 = int.Parse(Console.ReadLine());
+    }
+
+    //Creamos un método que pida el nombre y apellido del usuario
+    //y posteriormente lo salude con "Bienvenido [Apellido], [Nombre]
+    private static void PedirNombreYSaludar()
+    {
+        //Declaramos las variables
+        string nombre,apellido;
+
+        Console.Write("Ingrese su nombre:");
+        nombre = Console.ReadLine();
+
+        Console.Write("Ingrese su apellido:");
+        apellido = Console.ReadLine();
+
+        //Console.WriteLine("Bienvenido "+apellido+", "+nombre);
+        Console.WriteLine($"Bienvenido {apellido}, {nombre}");
+    }
+
+    /*Creamos un método que nos pida los años, meses y días de vida
+    y muestre al final la cantidad total de días de vida
+     */
+    private static void CalcularDiasDeVida()
+    {
+        //declaramos las variables
+        int años,meses,dias,diasDeVida;
+
+        Console.Write("Ingrese sus años de vida:");
+        años=int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese sus meses de vida:");
+        meses = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese sus días de vida:");
+        dias = int.Parse(Console.ReadLine());
+
+        diasDeVida = (años * 365) + (meses * 30) + dias;
+
+        Console.WriteLine($"Los días de vida totales son:{diasDeVida}");
+    }
+
+}
+
